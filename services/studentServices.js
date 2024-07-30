8dc8ae49c76999b2e77c8d058de3a41dfe55c4e2
prajwalkeerthi-rhibhus <prajwal.keerthi@rhibhus.com>
2024-07-30 14:26:13 +0530

    new changes

--- services/studentServices.js
+++ services/studentServices.js
@@ -11,7 +11,7 @@ const { response } = require("express");
 
 exports.fetchAllStudents = function (request, callback) {
     /** FETCH USER BY EMAIL **/
-    studentRepository.getStudentsData(request, function (fetch_teacher_section_students_err, fetch_teacher_section_students_response) {
+    studentRepository.getAllStudentsData(request, function (fetch_teacher_section_students_err, fetch_teacher_section_students_response) {
         if (fetch_teacher_section_students_err) {
             console.log(fetch_teacher_section_students_err);
             callback(fetch_teacher_section_students_err, fetch_teacher_section_students_response);
