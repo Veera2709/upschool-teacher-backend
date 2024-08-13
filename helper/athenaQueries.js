0a292c75a5c11a840d442630f3119fe111339d4c
prajwalkeerthi-rhibhus <prajwal.keerthi@rhibhus.com>
2024-08-13 17:14:57 +0530

    wrote athena query

--- helper/athenaQueries.js
+++ helper/athenaQueries.js
@@ -1,3 +1,5 @@
+const { TABLE_NAMES } = require("../constants/tables");
+
 exports.getQuizResultsQuery = (quizId) => `
   SELECT 
     CAST(q1.question_track_details AS json) AS question_track_details, 
@@ -7,3 +9,19 @@ exports.getQuizResultsQuery = (quizId) => `
     ON q1.quiz_id = q2.quiz_id
     WHERE q1.quiz_id = '${quizId}'
 `;
+
+exports.getQuizResults = (quizId) => `
+SELECT 
+    t1.student_id,
+    t1.user_firstname,
+    t2.individual_group_performance
+FROM 
+    ${TABLE_NAMES.upschool_student_info} t1
+JOIN 
+    ${TABLE_NAMES.upschool_quiz_result} t2
+ON 
+    t1.student_id = t2.student_id
+WHERE 
+    t2.quiz_id = '${quizId}';
+`;
+
