034b1109da71435f5ce6eae21e186fbd7773736d
yashwanth <yashwanth.Talagavadi@rhibhus.com>
2024-08-14 09:29:57 +0530

    U

--- helper/athenaQueries.js
+++ helper/athenaQueries.js
@@ -2,12 +2,16 @@ const { TABLE_NAMES } = require("../constants/tables");
 
 exports.getQuizResultsQuery = (quizId) => `
   SELECT 
-    CAST(q1.question_track_details AS json) AS question_track_details, 
-    CAST(q2.marks_details AS json) AS marks_details 
-    FROM dev_upschool_quiz_result q2
-    INNER JOIN dev_upschool_quiz_table q1
-    ON q1.quiz_id = q2.quiz_id
-    WHERE q1.quiz_id = '${quizId}'
+      CAST(q1.question_track_details AS json) AS question_track_details, 
+      CAST(q2.marks_details AS json) AS marks_details 
+  FROM 
+      ${TABLE_NAMES.upschool_quiz_table} q1
+  INNER JOIN 
+      ${TABLE_NAMES.upschool_quiz_result} q2
+  ON 
+      q1.quiz_id = q2.quiz_id
+  WHERE 
+      q1.quiz_id = '${quizId}'
 `;
 
 exports.getQuizResults = (quizId) => `
